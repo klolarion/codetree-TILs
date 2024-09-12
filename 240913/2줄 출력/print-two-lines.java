1a55e7cfc2e07dc1adfe42163d3da52cda7ecced
Klolarion <85093525+klolarion@users.noreply.github.com>
2024-09-13 00:05:12 +0900

    Title: 2줄 출력, Time: 94ms, Memory: 8MB, Status: Passed - CodeTree

--- 240913/2줄 출력/print-two-lines.java
+++ 240913/2줄 출력/print-two-lines.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
-        System.out.println("Java is Fun");
+        System.out.println("Hello");
+        System.out.println("World");
     }
 }
