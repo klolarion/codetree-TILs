46e4355a3277fd053c2049dfcde4040d2af63614
Klolarion <85093525+klolarion@users.noreply.github.com>
2024-09-12 21:50:57 +0900

    Title: 바이러스 검사, Time: 89ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240912/바이러스 검사/virus-detector.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));;  
+        
+        int restaurantCount = Integer.parseInt(bf.readLine());
+
+        String arr1[] = bf.readLine().split(" ");
+        int[] customers = new int[arr1.length];
+        for(int i=0; i<arr1.length; i++){
+            customers[i] = Integer.parseInt(arr1[i]);
+        }
+
+        String arr2[] = bf.readLine().split(" ");
+        int teamLeader = Integer.parseInt(arr2[0]);
+        int team = Integer.parseInt(arr2[1]);
+
+
+        int manNeed = 0;
+
+        for(int i=0; i<arr1.length; i++){
+            int tmp = customers[i] - teamLeader;
+            if(tmp<=0){
+                manNeed++;
+                continue;
+            }
+            int tmp2 = tmp/team;
+            manNeed += tmp2;
+
+            int plus = tmp%team;
+            if(plus > 0){
+                manNeed++;
+            }
+        }
+
+        bf.close();
+        System.out.println(manNeed);        
+    }
+}
