dda788c3aab5d2e3bc8f2e271f23171f84dbdb02
Klolarion <85093525+klolarion@users.noreply.github.com>
2024-09-13 00:04:26 +0900

    Title: 따옴표 출력, Time: 93ms, Memory: 8MB, Status: Passed - CodeTree

--- 240913/따옴표 출력/print-quote.java
+++ 240913/따옴표 출력/print-quote.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.our.println("He says \"It's a really simple sentence\".");
+        System.out.println("He says \"It's a really simple sentence\".");
     }
 }
