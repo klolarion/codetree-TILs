d8179735c1a443a9e8e79d9bfdabfb47bd91577a
Kim Jae Keun <85093525+klolarion@users.noreply.github.com>
2024-10-01 10:58:43 +0900

    Title: 정수 선언하고 뺄셈, Time: 106ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 241001/정수 선언하고 뺄셈/define-numbers-and-substract.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int i = 97;
+        int j = 13;
+        int r = i - j;
+        System.out.printf("%d - %d = %d", i, j, r);
+    }
+}
