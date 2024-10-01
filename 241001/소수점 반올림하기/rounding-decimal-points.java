67732ca44c79365b07b6ca6715b27ceb57fc95c8
Kim Jae Keun <85093525+klolarion@users.noreply.github.com>
2024-10-01 13:18:13 +0900

    Title: 소수점 반올림하기, Time: 90ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 241001/소수점 반올림하기/rounding-decimal-points.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        double d = 25.325;
+
+        double r = Math.round(d*10.0)/10.0;
+        System.out.printf("%.1f", d);
+    }
+}
