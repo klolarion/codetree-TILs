261f3b3cbe67fd09a47d94be8df593d66d063a49
Kim Jae Keun <85093525+klolarion@users.noreply.github.com>
2024-10-01 12:04:59 +0900

    Title: 달에서 무게 구하기, Time: 88ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 241001/달에서 무게 구하기/weight-on-the-moon.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 13;
+        double b = 0.165;
+        double r = a * b;
+
+        System.out.printf("%d * %f = %.6f", a, b, r);
+        
+    }
+}
