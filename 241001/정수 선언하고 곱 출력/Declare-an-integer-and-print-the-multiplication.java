c2d9ed28021594daee9c9881ee7308fb2111a6d7
Kim Jae Keun <85093525+klolarion@users.noreply.github.com>
2024-10-01 11:01:35 +0900

    Title: 정수 선언하고 곱 출력, Time: 91ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 241001/정수 선언하고 곱 출력/Declare-an-integer-and-print-the-multiplication.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 26;
+        int b = 5;
+        int r = a * b;
+        System.out.printf("%d * %d = %d", a, b, r);
+    }
+}
