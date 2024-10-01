d8a886ccbda09d4681bc0ad6a39c63c5df66448e
Kim Jae Keun <85093525+klolarion@users.noreply.github.com>
2024-10-01 18:57:22 +0900

    Title: 두 실수의 곱, Time: 95ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 241001/두 실수의 곱/the-product-of-two-real-numbers.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        double a = 5.26;
+        double b = 8.27;
+        
+        double r = a * b;
+
+        System.out.printf("%.3f", r);
+    }
+}
