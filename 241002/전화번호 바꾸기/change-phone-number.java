dcba09a2472d721dc919ae7602bc608621421dcf
Kim Jae Keun <85093525+klolarion@users.noreply.github.com>
2024-10-02 20:57:17 +0900

    Title: 전화번호 바꾸기, Time: 239ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241002/전화번호 바꾸기/change-phone-number.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        sc.useDelimiter("-");
+        String a = sc.next();
+        String b = sc.next();
+        String c = sc.next();        
+
+        System.out.printf("%s-%s-%s", a, c, b);
+    }
+}
