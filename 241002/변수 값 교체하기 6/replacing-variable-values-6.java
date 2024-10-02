e2d90dfa781cb5f1ab326e8957401e7df2608bc3
Kim Jae Keun <85093525+klolarion@users.noreply.github.com>
2024-10-02 12:02:37 +0900

    Title: 변수 값 교체하기 6, Time: 88ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 241002/변수 값 교체하기 6/replacing-variable-values-6.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+       int a = 2;
+       int b = 5;
+       int tmp = 0;
+
+       tmp = a;
+       a = b;
+       b = tmp;
+
+       System.out.println(a);
+       System.out.println(b);
+    }
+}
