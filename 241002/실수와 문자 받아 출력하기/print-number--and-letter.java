839eee07172442244f91f36205d3d5f727cd5531
Kim Jae Keun <85093525+klolarion@users.noreply.github.com>
2024-10-02 19:52:25 +0900

    Title: 실수와 문자 받아 출력하기, Time: 120ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241002/실수와 문자 받아 출력하기/print-number--and-letter.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        char c = sc.next().charAt(0);
+
+
+
+
+
+        double d = Double.parseDouble(sc.next());
+
+        double e = Double.parseDouble(sc.next());
+
+
+
+        System.out.println(c);
+        System.out.printf("%.2f\n", d);
+        System.out.printf("%.2f", e);
+    }
+}
