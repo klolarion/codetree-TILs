a929df8e63cd92155abf527223242f80227132ed
Kim Jae Keun <85093525+klolarion@users.noreply.github.com>
2024-10-02 20:47:14 +0900

    Title: 날짜 변경하여 출력 2, Time: 120ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 241002/날짜 변경하여 출력 2/print-date-with-different-format-2.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        sc.useDelimiter("-");
+        String y = sc.next();
+        String m = sc.next();
+        String d = sc.next();
+
+        System.out.printf("%s.$s.%s", y,m,d);
+    }
+}
