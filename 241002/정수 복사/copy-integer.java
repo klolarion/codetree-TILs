0c8649223d60c66b4fbe6f735a37832f2aca8882
Kim Jae Keun <85093525+klolarion@users.noreply.github.com>
2024-10-02 12:00:07 +0900

    Title: 정수 복사, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 241002/정수 복사/copy-integer.java
+++ 241002/정수 복사/copy-integer.java
@@ -3,6 +3,7 @@ public class Main {
         int a = 3;
         int b = 4;
         b = a;
-        System.out.println(a*b);
+        System.out.printf("%d %d", a b);
+        System.out.println(a * b);
     }
 }
