e3c0ea55dc2a5ce24d6af4552e196c208f4eed76
Kim Jae Keun <85093525+klolarion@users.noreply.github.com>
2024-10-02 19:47:59 +0900

    Title: 문자 받아 출력, Time: 112ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241002/문자 받아 출력/enter-char-and-print.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        char c = s.charAt(0);
+
+        System.out.print(c);
+    }
+}
