de0411cf617c2f54f84f0b98d25137baecaf0341
Kim Jae Keun <85093525+klolarion@users.noreply.github.com>
2024-10-02 22:11:27 +0900

    Title: 간단한 사칙연산, Time: 117ms, Memory: 10MB, Status: Passed - CodeTree

--- 241002/간단한 사칙연산/simple-arithmetic-operation.java
+++ 241002/간단한 사칙연산/simple-arithmetic-operation.java
@@ -3,7 +3,6 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        sc.useDelimiter(" ");
         int a = sc.nextInt();
         int b = sc.nextInt();
 
