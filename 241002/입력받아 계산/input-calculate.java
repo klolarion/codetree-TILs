35b8413d155ff9d2639a301650d17bbb1127c760
Kim Jae Keun <85093525+klolarion@users.noreply.github.com>
2024-10-02 12:07:07 +0900

    Title: 입력받아 계산, Time: 129ms, Memory: 10MB, Status: Passed - CodeTree

--- 241002/입력받아 계산/input-calculate.java
+++ 241002/입력받아 계산/input-calculate.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextIne();
+        int a = sc.nextInt();
         System.out.println(a+2);
     }
 }
