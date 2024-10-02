3187fee5172b6806ceb3524dc63f21bff2b8d9ed
Kim Jae Keun <85093525+klolarion@users.noreply.github.com>
2024-10-03 00:04:04 +0900

    Title: 입력받아 연산하기, Time: 121ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241003/입력받아 연산하기/take-input-and-operate.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt() + 87;
+        int b = sc.nextInt() % 10;
+
+        System.out.printf("%d\n", a);
+        System.out.printf("%d\n", b);
+    }
+}
