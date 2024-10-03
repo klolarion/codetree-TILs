48de4bc30e6ab95c09a313dda25f69d4c9135ab8
Kim Jae Keun <85093525+klolarion@users.noreply.github.com>
2024-10-04 02:42:45 +0900

    Title: 최소 차이로 묶기, Time: 90ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 241004/최소 차이로 묶기/bundling-with-minimum-difference.java
+++ 241004/최소 차이로 묶기/bundling-with-minimum-difference.java
@@ -9,31 +9,42 @@ public class Main {
         int n = Integer.parseInt(st.nextToken()); // 수열 a의 원소 개수
         int m = Integer.parseInt(st.nextToken()); // 수열 b의 원소 개수
 
-        int[] a = new int[n];
-        int[] b = new int[m];
+        List<Integer> a = new ArrayList<>();
+        List<Integer> b = new ArrayList<>();
 
         // 수열 a 입력
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
-            a[i] = Integer.parseInt(st.nextToken());
+            a.add(Integer.parseInt(st.nextToken()));
         }
 
         // 수열 b 입력
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < m; i++) {
-            b[i] = Integer.parseInt(st.nextToken());
+            b.add(Integer.parseInt(st.nextToken()));
         }
 
         // 수열 a와 b 정렬
-        Arrays.sort(a);
-        Arrays.sort(b);
+        Collections.sort(a);
+        Collections.sort(b);
 
         // 두 수열에서 쌍을 만들어 차이의 합을 계산
         int minPairs = Math.min(n, m); // 만들 수 있는 최대 쌍의 개수(작은수만큼 만들 수 있음)
         int totalDifference = 0;
 
-        for (int i = 0; i < minPairs; i++) {
-            totalDifference += Math.abs(a[i] - b[i]);
+        for (int i = 0; i < a.size(); i++) {
+            int tmp = Integer.MAX_VALUE;
+            int low_j = 0;
+            for(int j = 0; j< b.size(); j++){
+                if(Math.abs(a.get(i) - b.get(i)) < tmp){
+                    tmp = Math.abs(a.get(i) - b.get(i));
+                    low_j = j;
+                }
+            }
+            a.remove(i);
+            b.remove(low_j);
+            totalDifference += tmp;
+            
         }
 
         // 결과 출력
