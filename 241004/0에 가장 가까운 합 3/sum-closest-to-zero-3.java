a9f79413843a4e27277613dfde81ee746c6e7057
Kim Jae Keun <85093525+klolarion@users.noreply.github.com>
2024-10-04 00:53:25 +0900

    Title: 0에 가장 가까운 합 3, Time: 3039ms, Memory: 34MB, Status: Time Limit Exceed - CodeTree

--- 241004/0에 가장 가까운 합 3/sum-closest-to-zero-3.java
+++ 241004/0에 가장 가까운 합 3/sum-closest-to-zero-3.java
@@ -1,21 +1,13 @@
 import java.util.*;
-import java.io.*;
 
 
 public class Main {
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        // 입력 첫 줄에서 숫자의 개수 받기
-        int n = Integer.parseInt(br.readLine());
-        
-        
-         // 입력 데이터를 한 줄로 받아서 공백으로 분리
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        
-        int[] list = new int[n]; // 고정 크기의 배열 사용
-        for (int i = 0; i < n; i++) {
-            list[i] = Integer.parseInt(st.nextToken());
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        List<Integer> list = new ArrayList<>();
+        while(sc.hasNext()){
+            list.add(sc.nextInt());
         }
 
 
@@ -23,11 +15,11 @@ public class Main {
         int result = 0;
         int min = Integer.MAX_VALUE;
         
-        for(int i=0; i<list.length; i++){
-            int a = list[i];
+        for(int i=0; i<list.size(); i++){
+            int a = list.get(i);
             
-            for(int j=1; j<list.length; j++){
-                int b = list[j];
+            for(int j=1; j<list.size(); j++){
+                int b = list.get(j);
                 result = Math.abs(a+b);
                 if(result < Math.abs(min)){
                     min = a+b;
